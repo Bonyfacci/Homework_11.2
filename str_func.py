22dd9a313865d8dbcda46f44a20eb79501c5b913
Сергей Платонов <platonovpochta@gmail.com>
2023-05-11 23:28:16 +0200

    add def string_title in file str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def string_upper(text):
     return text.upper()
+
+def string_title(text):
+    """
+    Возвращает строку с заглавными первыми буквами каждого слова в строке.
+    :param text: str
+    :return: str
+    """
+    return text.title()
